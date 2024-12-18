ccbf49069f692f14439510fd4790fe8ee78dffe7
GintokiSa84 <dulygin18@gmail.com>
2024-12-18 20:15:02 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var=21, 54 ,"two"
+print(immutable_var)
+#immutable_var[0]=24 #нельзя изменять даные в кортеже
+mutable_list=[21, 54,"two"]
+mutable_list[0] = 24
+mutable_list.append("free")
+print(mutable_list)
