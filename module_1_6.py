b74cbd4a51fde61a7a35c49b64d65adc78aacd54
GintokiSa84 <dulygin18@gmail.com>
2024-12-19 21:32:08 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict={ 'Anna': 1998, 'Jana': 1989 }
+my_dict['Egor']=1997
+my_dict.update({'Sahsa':1995,
+                 'Ivan':1979})
+print(my_dict)
+print(my_dict.get('Vova'))
+print(my_dict['Anna'])
+print(my_dict.pop('Ivan'))
+
+my_set={21, 23, 21, "ad48", 50, 58,50,}
+print(my_set)
+my_set.add((590, "dsd")),(24)
+my_set.discard(58)
+print(my_set)
